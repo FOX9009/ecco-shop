179e9a80f164be974be63568fa87d0e2101bf40d
FOX <lisichenko9009@mail.ru>
2022-09-19 20:14:36 +0700

    база js

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function sum(a,b){
 }
 
 
+var summa = (a,b) => a+b;
+
+var result = summa(1,4)
+
+
+
+var ar = [1,2,3,4];
+
+var newArr = ar.map(function(i){
+    return i*i
+})
+
+//////////////////Запись через стрелочную функцию
+
+var newArrStr = ar.map(i => i*i)
+
+//var - публичная переменная со всплытием
+//let - переменная без всплытия
+//const - постоянная констранта, не способная к изменению
